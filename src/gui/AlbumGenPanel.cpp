69ccaea75d033b4cfc79fbc5110a3f59b267d7a0
EddieM0710 <Eddiem0710@gmail.com>
2022-08-01 16:54:31 -0500

    Added License Statement

--- src/gui/AlbumGenPanel.cpp
+++ src/gui/AlbumGenPanel.cpp
@@ -6,6 +6,19 @@
  * @date 2021-02-25
  *
  * @copyright Copyright (c) 2021
+ * 
+ * This file is part of AlbumGenerator.
+ *
+ * AlbumGenerator is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or any later version.
+ *
+ * AlbumGenerator is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * AlbumGenerator. If not, see <https://www.gnu.org/licenses/>.
  *
  **************************************************/
 
@@ -234,8 +247,8 @@ void AlbumGenPanel::OnNotebookPageChanged( wxNotebookEvent& event )
 
 void AlbumGenPanel::OnTitleTextUpdated( wxCommandEvent& event )
 {
-    ////@begin wxEVT_COMMAND_TEXT_UPDATED event handler for ID_TITLETEXTCTRL in AlbumGenPanel.
+    // wxEVT_COMMAND_TEXT_UPDATED event handler for ID_TITLETEXTCTRL in AlbumGenPanel.
         // Before editing this code, remove the block markers.
     event.Skip( );
-    ////@end wxEVT_COMMAND_TEXT_UPDATED event handler for ID_TITLETEXTCTRL in AlbumGenPanel. 
+    // wxEVT_COMMAND_TEXT_UPDATED event handler for ID_TITLETEXTCTRL in AlbumGenPanel. 
 }
